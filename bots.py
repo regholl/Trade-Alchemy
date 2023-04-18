86a1da6993799e8e5256babb54323cd40ccec8cf
AltKrypto <84981131+AltKrypto@users.noreply.github.com>
2023-04-18 19:13:32 -0400

    omit live and storage from bots
    This is a testing safeguard

--- bots.py
+++ bots.py
@@ -1,7 +1,5 @@
 import models.paper as pa
 import models.data as dt
-import models.live as lv
-import models.storage as st
 
 
 #							GRID TRADING BOT
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import models.data as dt
 import models.paper as pa
 import models.live as lv
 import models.storage as st
+
+
