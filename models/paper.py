2c47f626c1b7ee30db4475ff23bd13f0d63c0b44
AltKrypto <84981131+AltKrypto@users.noreply.github.com>
2023-04-24 06:49:22 -0400

    remove order validation
    Seemed unnecessary, orders are not dynamically generated but instead
    are programmatically designed

--- models/paper.py
+++ models/paper.py
@@ -1,4 +1,3 @@
-from marshmallow import Schema, fields, validate
 from dotenv import load_dotenv
 import requests
 import json
@@ -26,58 +25,6 @@ headers = {
  'Apca-Api-Key-Id': paper_api,
  'Apca-Api-Secret-Key': paper_secret
 }
-
-
-#							VALIDATION
-#########################################################
-
-# Define the order schema for stocks using marshmallow
-class StockOrderSchema(Schema):
-	symbol = fields.Str(required=True)
-	qty = fields.Integer(required=True, validate=validate.Range(min=1))
-	side = fields.Str(required=True, validate=validate.OneOf(["buy", "sell"]))
-	type = fields.Str(required=True, validate=validate.OneOf(["market", "limit", "stop", "stop_limit"]))
-	time_in_force = fields.Str(required=True, validate=validate.OneOf(["gtc", "opg", "ioc", "fok"]))
-	limit_price = fields.Float(validate=validate.Range(min=0))
-	stop_price = fields.Float(validate=validate.Range(min=0))
-	trail_price = fields.Float(validate=validate.Range(min=0))
-	client_order_id = fields.Str(validate=validate.Length(max=48))
-	order_class = fields.Str(validate=validate.OneOf(["simple", "bracket", "oco", "oto"]))
-	take_profit = fields.Float(validate=validate.Range(min=0))
-	stop_loss = fields.Float(validate=validate.Range(min=0))
-	
-# Define the order schema for crypto using marshmallow
-class CryptoOrderSchema(Schema):
-	symbol = fields.Str(required=True)
-	qty = fields.Integer(required=True, validate=validate.Range(min=1))
-	side = fields.Str(required=True, validate=validate.OneOf(["buy", "sell"]))
-	type = fields.Str(required=True, validate=validate.OneOf(["market", "limit", "stop_limit"]))
-	time_in_force = fields.Str(required=True, validate=validate.OneOf(["gtc", "ioc"]))
-	limit_price = fields.Float(validate=validate.Range(min=0))
-	stop_price = fields.Float(validate=validate.Range(min=0))
-	trail_price = fields.Float(validate=validate.Range(min=0))
-	client_order_id = fields.Str(validate=validate.Length(max=48))
-	order_class = fields.Str(validate=validate.OneOf(["simple", "bracket", "oco", "oto"]))
-	take_profit = fields.Float(validate=validate.Range(min=0))
-	stop_loss = fields.Float(validate=validate.Range(min=0))
-	
-# this function validates stock orders
-def validate_stock_order(order):
-	order_schema = StockOrderSchema()
-	errors = order_schema.validate(order)
-	if errors:
-		print(errors)  # Handle validation errors
-	else:
-		return order
-
-# this function validates crypto orders
-def validate_crypto_order(order):
-	order_schema = CryptoOrderSchema()
-	errors = order_schema.validate(order)
-	if errors:
-		print(errors)  # Handle validation errors
-	else:
-		return order
 
 
 #                                                       METHODS
